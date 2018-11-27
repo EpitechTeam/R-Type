7eb4d5c31387877ef58866b4761a91060d70024a
Kahoul <ilyes.kahoul@epitech.eu>
2018-11-27 17:14:03 +0100

    [Done] Repo refactoring

--- src/test/compile_client.sh
+++
@@ -1,2 +0,0 @@
-g++ -I ../../include -L ../../lib/ -Lbuild chat_client.cpp -lboost_thread -lpthread -lboost_regex -lboost_system -o client
-export LD_LIBRARY_PATH=../../lib
--- src/test/compile_server.sh
+++
@@ -1,2 +0,0 @@
-g++ -I ../../include -L ../../lib/ -Lbuild chat_server.cpp -lboost_thread -lpthread -lboost_regex -lboost_system -o server
-export LD_LIBRARY_PATH=../../lib
