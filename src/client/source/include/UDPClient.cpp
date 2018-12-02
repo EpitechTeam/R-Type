f274fceeef8bfcd77f29c072b2e8cc08b54e316a
David Zakrzewski <david.zakrzewski@epitech.eu>
2018-12-02 18:35:15 +0100

    Oups

--- src/client/source/include/UDPClient.cpp
+++ src/client/source/include/UDPClient.cpp
@@ -8,6 +8,7 @@ UDPClient::UDPClient(boost::asio::io_context &io_context, const std::string &ip,
 	_receiverEndpoint = new udp::endpoint(address::from_string(ip), port);
 	_socket = new udp::socket(io_context);
 	_socket->open(udp::v4());
+	//std::cout << "listener started\n";
 	_updListenenerThread = new std::thread([this]() {
 		startListener();
 	});
@@ -25,6 +26,7 @@ void UDPClient::startListener()
 {
 	_type = 0;
 
+	//std::cout << "\tListening in thread...\n";
 	_socket->async_receive_from(boost::asio::buffer(_listenerRecvBuffer),
 			_listenerSenderEndpoint,
 			boost::bind(&UDPClient::handleReceive,
@@ -37,25 +39,38 @@ void UDPClient::startListener()
 void UDPClient::request(std::string msg, std::function<void(std::string)> callback) {
 
 	_type = 1;
+	boost::shared_ptr<std::string> message(new std::string(msg));
 
-	boost::shared_ptr<std::string>	message(new std::string(msg));
 
 	_socket->async_send_to(boost::asio::buffer(*message), *_receiverEndpoint,
-						   boost::bind(&UDPClient::handleSend, this,
+						   boost::bind(&UDPClient::handleSend, this, message,
 									   boost::asio::placeholders::error,
 									   boost::asio::placeholders::bytes_transferred));
-	callback(std::string(_recvBuffer));
+	callback(_recvBuffer.data());
+	_recvBuffer.assign(0);
 }
 
 void UDPClient::handleReceive(const boost::system::error_code& error,
 							  std::size_t bytes_transferred) {
 	if (!error || error == boost::asio::error::message_size) {
 		if (_type == 0) {
+            //std::cout << "Receive some packets.... == 0\n";
+
+            if (_listenerRecvBuffer.data()[0] == 'D') {
+                std::cout << "Receive some packets.... == 0  " << _listenerRecvBuffer.data() << std::endl;
+            }
 			_game->updateView(std::string(_listenerRecvBuffer.data()));
 		}
 		else {
+			// Reçu du server
+			//if (_listenerRecvBuffer.data().at(0) == 'D') {
+                //std::cout << "Receive some packets.... != 0  " << _listenerRecvBuffer.data() << std::endl;
+			//}
 			_game->updateView(std::string(_recvBuffer.data()));
+//			std::cout << "response: " << _recvBuffer.data() << std::endl;
 		}
+        _listenerRecvBuffer.assign(0);
+        _recvBuffer.assign(0);
         startListener();
 	}
 }
@@ -65,7 +80,7 @@ void UDPClient::handleSend(boost::shared_ptr<std::string> message,
 						   std::size_t bytes_transferred) {
 	if (!error || error == boost::asio::error::message_size) {
 		_socket->async_receive_from(
-				boost::asio::buffer(boost::asio::buffer(_recvBuffer)), _senderEndpoint,
+				boost::asio::buffer(_recvBuffer), _senderEndpoint,
 				boost::bind(&UDPClient::handleReceive, this,
 							boost::asio::placeholders::error,
 							boost::asio::placeholders::bytes_transferred));
