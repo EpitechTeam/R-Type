f867870d55156c7f97bb3cc4f02208ee6b164ed4
lecherbonnier <leo.lecherbonnier@epitech.eu>
2018-12-02 16:07:20 +0100

    FIX: Change player bar

--- src/client/source/lib/Room.cpp
+++ src/client/source/lib/Room.cpp
@@ -236,10 +236,10 @@ void Room::draw(sf::RenderWindow *window) {
     window->draw(this->exit_room);
     window->draw(this->starship);
     front_promt.setFillColor(sf::Color::White);
-    for(unsigned int i = 0; this->player.size() != i; i++) {
+    for(unsigned int i = 0; this->player.size() - 1 != i; i++) {
         tag.setPosition(1290, 100 + (i * 60));
         window->draw(tag);
-        front_promt.setString("player"+ std::to_string(i + 1) + ": " + player[i]);
+        front_promt.setString(std::to_string(i + 1) + ": " + player[i]);
         front_promt.setPosition(900, 115 + (i * 60));
         window->draw(this->front_promt);
     }
